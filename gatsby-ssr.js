4bea5f5ae5ecc71b7f45c398d440d3b341748ebf
Hamza khan <hamza@blackandblackcreative.com>
2023-11-04 03:23:12 +0500

    fxd os 5

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { ThemeProviderr } from "./src/styles/themeContext";
+import { ServerStyleSheet } from "styled-components";
 
 export const wrapRootElement = ({ element }) => {
     return (
@@ -9,24 +10,29 @@ export const wrapRootElement = ({ element }) => {
     );
 };
 
+
 const InsertScript = () => {
     const codeToRunOnClient = () => {
         (function () {
-            var storageKey = "darkMode";
+            var storageKey = "theme";
             var classNameDark = "dark-mode";
             var classNameLight = "light-mode";
             var element = window.document.documentElement;
             var localStorageTheme = null;
             try {
                 localStorageTheme = localStorage.getItem(storageKey);
+                console.log("localStorageTheme", localStorageTheme); // this works
             } catch (err) {}
             var localStorageExists = localStorageTheme !== null;
             if (localStorageExists) {
                 element.classList.add(
-                    localStorageTheme === "true"
-                        ? classNameDark
-                        : classNameLight
+                    localStorageTheme === "true" ? classNameDark : classNameLight
                 );
+                // add to styled components theme here too
+                var styleSheet = new ServerStyleSheet();
+                var theme = localStorageTheme === "true" ? "dark" : "light";
+                var style = `:root { --theme: ${theme}; }`;
+                styleSheet.collectStyles(<style>{style}</style>);
             } else {
                 var isDarkMode = element.classList.contains(classNameDark);
                 localStorage.setItem(storageKey, isDarkMode);
@@ -35,8 +41,8 @@ const InsertScript = () => {
     };
     const calledFunction = `(function(){${codeToRunOnClient.toString()}})();`;
     return <script dangerouslySetInnerHTML={{ __html: calledFunction }} />;
-}
+};
 
 export const onRenderBody = ({ setPreBodyComponents }) => {
     setPreBodyComponents(<InsertScript />);
-}
+};
